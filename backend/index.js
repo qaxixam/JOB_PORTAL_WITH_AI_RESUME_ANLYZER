321f53f0e5614092727873314ed4f0b7d21f87f1
Qazi Zaid <zaidqazi257@gmail.com>
2026-07-15 20:59:35 +0500

    Fix formatting issues in index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://job-portal-with-ai-resume-anlyzer-1.onrender.com/',
+    origin: 'https://job-portal-with-ai-resume-anlyzer-1.onrender.com',
     credentials: true
 }
 
